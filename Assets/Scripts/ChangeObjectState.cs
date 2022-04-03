3b83fb343c18bd895c402c974b0821a669a10f81
Lucio Misael Aquino <luq.notperfect@gmail.com>
2022-04-03 10:04:58 -0300

    Added house, lights, interactable objects, sound manager and events to trigger actions

---
+++ Assets/Scripts/ChangeObjectState.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ChangeObjectState : MonoBehaviour
+{
+    public string ID;
+    public bool StartNormalState = true;
+    public string NormalStateSound;
+    public List<GameObject> NormalStateObjects;
+    public string SecondaryStateSound;
+    public List<GameObject> SecondaryStateObjects;
+
+    private void OnEnable()
+    {
+        GameEvents.OnChangeObjectState += ChangeState;
+        if (StartNormalState)
+            SetNormalState();
+        else
+            SetSecondaryState();
+    }
+    private void OnDisable()
+    {
+        GameEvents.OnChangeObjectState -= ChangeState;
+    }
+
+    private void ChangeState(string id)
+    {
+        if(id == ID)
+        {
+            if (StartNormalState)
+                SetSecondaryState();
+            else
+                SetNormalState();
+        }
+    }
+
+    public void SetNormalState()
+    {
+        SecondaryStateObjects.ForEach(x => x.SetActive(false));
+        NormalStateObjects.ForEach(x => x.SetActive(true));
+
+        if (string.IsNullOrEmpty(NormalStateSound))
+            GameEvents.PlaySound(NormalStateSound);
+    }
+    public void SetSecondaryState()
+    {
+        NormalStateObjects.ForEach(x => x.SetActive(false));
+        SecondaryStateObjects.ForEach(x => x.SetActive(true));
+
+        if (string.IsNullOrEmpty(SecondaryStateSound))
+            GameEvents.PlaySound(SecondaryStateSound);
+    }
+}
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+
+public class SoundManager : MonoBehaviour {
+
+	public List<AudioSource> Sounds;
+    private void Awake()
+    {
+		GameEvents.OnPlaySound += PlaySound;
+		GameEvents.OnStopSound += StopSound;
+
+		ConfigSounds();
+	}
+	private void OnDisable()
+	{
+		GameEvents.OnPlaySound -= PlaySound;
+		GameEvents.OnStopSound -= StopSound;
+	}
+
+
+
+	private void PlaySound(string soundName, bool loop)
+	{
+		var sound = FindAudio(soundName);
+		if (sound.isPlaying)
+			sound.Stop();
+		sound.loop = loop;
+		sound.Play();
+	}
+
+	private void StopSound(string soundName)
+	{
+		var sound = FindAudio(soundName);
+		sound.Stop();
+	}
+
+	private AudioSource FindAudio(string soundName)
+    {
+		return Sounds.FirstOrDefault(x => x.clip.name == soundName);
+    }
+
+	[ContextMenu("SetDefaultSettings")]
+	public void ConfigSounds()
+	{
+		Sounds.Clear();
+        for (int i = 0; i < transform.childCount; i++)
+        {
+			var child = transform.GetChild(i).GetComponentInChildren<AudioSource>();
+			if (child != null && child.clip != null)
+			{
+				child.name = child.clip.name;
+				child.playOnAwake = false;
+				child.loop = false;
+				Sounds.Add(child);
+			}
+			else
+				child.name = "ERROR! :: (missing audio clip)";
+		}
+	}
+}
